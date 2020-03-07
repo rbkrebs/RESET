ec5f881d6ccee09258b79f76cda9e9a8404a2271
Romulo Krebs <romulo.krebs@gmail.com>
2020-03-07 15:46:03 -0300

    criacao classe calculadora

--- src/App.java
+++ src/App.java
@@ -3,8 +3,16 @@ public class App {
     public static void main(String[] args) {
 
 
-        System.out.printf("Hello mundo!");
+        System.out.println("Hello mundo!");
         System.out.println("Uma modificação");
-        System.out.println("Modificado do Github");
+
+        int valor1 = 12;
+        int valor2 = 13;
+
+        Calculadora calc = new Calculadora();
+        System.out.println("Somar "+valor1+" e "+valor2+" = "+calc.somar(valor1,valor2));
+        System.out.println("Subtrair "+valor1+" e "+valor2+" = "+calc.subtrair(valor1,valor2));
+        System.out.println("Multiplicar "+valor1+" e "+valor2+" = "+calc.multiplicar(valor1,valor2));
+        System.out.println("Dividir "+valor1+" e "+valor2+" = "+calc.dividir(valor1,valor2));
     }
 }
---
+++ src/Calculadora.java
@@ -0,0 +1,25 @@
+public class Calculadora {
+
+
+    int somar (int valor1, int valor2){
+
+        return valor1 + valor2;
+    }
+    int subtrair (int valor1, int valor2){
+
+        return valor1 - valor2;
+    }
+    int multiplicar (int valor1, int valor2){
+
+        return valor1 * valor2;
+    }
+
+    int dividir (int valor1, int valor2){
+
+        return valor1 / valor2;
+    }
+
+
+
+
+}
