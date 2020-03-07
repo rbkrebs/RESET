0f493c7dd47164bf25d56096410a59bbeea0fbc0
Romulo Krebs <romulo.krebs@gmail.com>
2020-03-07 16:09:13 -0300

    criacao classe comparador

--- src/App.java
+++ src/App.java
@@ -6,13 +6,15 @@ public class App {
         System.out.println("Hello mundo!");
         System.out.println("Uma modificação");
 
-        int valor1 = 12;
+        int valor1 = 14;
         int valor2 = 13;
 
         Calculadora calc = new Calculadora();
+        Comparador comp = new Comparador();
         System.out.println("Somar "+valor1+" e "+valor2+" = "+calc.somar(valor1,valor2));
         System.out.println("Subtrair "+valor1+" e "+valor2+" = "+calc.subtrair(valor1,valor2));
         System.out.println("Multiplicar "+valor1+" e "+valor2+" = "+calc.multiplicar(valor1,valor2));
         System.out.println("Dividir "+valor1+" e "+valor2+" = "+calc.dividir(valor1,valor2));
+        System.out.println("Comparar "+valor1+" menor que "+valor2+" = "+comp.menorQue(valor1,valor2));
     }
 }
---
+++ src/Comparador.java
@@ -0,0 +1,7 @@
+public class Comparador {
+
+    boolean menorQue (int valor1, int valor2){
+
+        return valor1<valor2;
+    }
+}
